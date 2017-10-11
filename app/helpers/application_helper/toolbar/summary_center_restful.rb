46cc4487ac44f8d7804867d237f1f15ebe03103b
Douglas Gabriel <douglasxgabriel@gmail.com>
2017-10-11 16:41:03 -0300

    adding operations buttons at the 'Providers' -> 'Physical Servers' page

--- app/helpers/application_helper/toolbar/summary_center_restful.rb
+++ app/helpers/application_helper/toolbar/summary_center_restful.rb
@@ -11,4 +11,93 @@ class ApplicationHelper::Toolbar::SummaryCenterRestful < ApplicationHelper::Tool
       :url_parms => "?id=\#{@record.id}",
       :klass     => ApplicationHelper::Button::ShowSummary),
   ])
+
+
+  button_group(
+    'physical_server_operations',
+    [
+      select(
+        :physical_server_power_choice,
+        'fa fa-power-off fa-lg',
+        N_('Power Functions'),
+        N_('Power'),
+        :items => [
+          button(
+            :physical_server_power_on,
+            nil,
+            N_('Power on the server'),
+            N_('Power On'),
+            :image   => "power_on",
+            :data    => {'function'      => 'sendDataWithRx',
+                         'function-data' => '{"type": "power_on", "controller": "physicalServerToolbarController"}'},
+            :confirm => N_("Power on the server?"),
+            :options => {:feature => :power_on}
+          ),
+          button(
+            :physical_server_power_off,
+            nil,
+            N_('Power off the server'),
+            N_('Power Off'),
+            :image   => "power_off",
+            :data    => {'function'      => 'sendDataWithRx',
+                         'function-data' => '{"type": "power_off", "controller": "physicalServerToolbarController"}'},
+            :confirm => N_("Power off the server?"),
+            :options => {:feature => :power_off}
+          ),
+          button(
+            :physical_server_restart,
+            nil,
+            N_('Restart the server'),
+            N_('Restart'),
+            :image   => "power_reset",
+            :data    => {'function'      => 'sendDataWithRx',
+                         'function-data' => '{"type": "restart", "controller": "physicalServerToolbarController"}'},
+            :confirm => N_("Restart the server?"),
+            :options => {:feature => :restart}
+          ),
+        ]
+      ),
+      select(
+        :physical_server_identify_choice,
+        nil,
+        N_('Identify LED Operations'),
+        N_('Identify'),
+        :items => [
+          button(
+            :physical_server_blink_loc_led,
+            nil,
+            N_('Blink the Identify LED'),
+            N_('Blink LED'),
+            :image   => "blank_button",
+            :data    => {'function'      => 'sendDataWithRx',
+                         'function-data' => '{"type": "blink_loc_led", "controller": "physicalServerToolbarController"}'},
+            :confirm => N_("Blink the Identify LED?"),
+            :options => {:feature => :blink_loc_led}
+          ),
+          button(
+            :physical_server_turn_on_loc_led,
+            nil,
+            N_('Turn on the Idenfity LED'),
+            N_('Turn On LED'),
+            :image   => "blank_button",
+            :data    => {'function'      => 'sendDataWithRx',
+                         'function-data' => '{"type": "turn_on_loc_led", "controller": "physicalServerToolbarController"}'},
+            :confirm => N_("Turn on the Identify LED?"),
+            :options => {:feature => :turn_on_loc_led}
+          ),
+          button(
+            :physical_server_turn_off_loc_led,
+            nil,
+            N_('Turn off the Identify LED'),
+            N_('Turn Off LED'),
+            :image   => "blank_button",
+            :data    => {'function'      => 'sendDataWithRx',
+                         'function-data' => '{"type": "turn_off_loc_led", "controller": "physicalServerToolbarController"}'},
+            :confirm => N_("Turn off the Identify LED?"),
+            :options => {:feature => :turn_off_loc_led}
+          ),
+        ]
+      ),
+    ]
+  )
 end
